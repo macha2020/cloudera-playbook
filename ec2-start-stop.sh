1854f824e178d795f3082110f28cc0075943b51f
macha <cloud_user@mrciti7c.mylabserver.com>
2020-08-12 22:12:35 +0000

    added ad-hoc scripts to delete and create 10 nodes to fit for this case, this will be handy to recreate ec2 instances with different AMIIDs during testing. However, this will be enhanced further into a standard script or playbook to control (create/delete/start/stop) ec2 instances of a given cdh cluster with necessary controls for deletions and terminations

---
+++ ec2-start-stop.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+function usage() {
+  echo "$0 (start|stop|status)"
+}
+
+if [ $# -ne 1 ]; then
+  usage
+  exit 2
+fi
+
+DEBUG="echo "
+DEBUG=""
+
+INSTANCE_ID=i-XXXXXXXX
+REGION=XXXXXXXX
+
+INSTANCE_STS=`aws ec2 describe-instance-status --region $REGION --instance-ids $INSTANCE_ID | jq -r '.InstanceStatuses[].InstanceState.Name'`
+
+echo "InstanceStatus : [$INSTANCE_STS]"
+
+EXIT=0
+if [ "$1" = "start" ]; then
+  if [ "$INSTANCE_STS" = "running" ]; then
+    echo "instance is already running."
+  else
+    echo "start instance..."
+    $DEBUG aws ec2 start-instances --instance-ids $INSTANCE_ID --region $REGION
+    EXIT=$?
+  fi
+elif [ "$1" = "stop" ]; then
+  if [ "$INSTANCE_STS" = "running" ]; then
+    echo "stop instance..."
+    $DEBUG aws ec2 stop-instances --instance-ids $INSTANCE_ID --region $REGION
+    EXIT=$?
+  else
+    echo "instance is already stopped."
+  fi
+elif [ "$1" = "status" ]; then
+  echo "Status Check only."
+else
+  usage
+  EXIT=2
+fi
+
+exit $EXIT
+
